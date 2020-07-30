f5473fda8021ba086321a850785eacf5943b4c74
Stanley J. Muzhuthettu <smuzh001@ucr.edu>
2020-07-30 13:44:18 -0700

    added create and get api aswell as endpoints and tests

--- tests/handler.test.js
+++ tests/handler.test.js
@@ -1,12 +1,18 @@
 import * as handler from '../handler';
 
-test('hello', async () => {
-  const event = 'event';
-  const context = 'context';
-  const callback = (error, response) => {
-    expect(response.statusCode).toEqual(200);
-    expect(typeof response.body).toBe("string");
-  };
+try{
+  test('hello', async () => {
+    const event = 'event';
+    const context = 'context';
+    const callback = (error, response) => {
+      expect(response.statusCode).toEqual(200);
+      expect(typeof response.body).toBe("string");
+    };
+  
+    await handler.hello(event, context, callback);
+  });
+  
+} catch(e){
+  console.log(e);
+}
 
-  await handler.hello(event, context, callback);
-});
